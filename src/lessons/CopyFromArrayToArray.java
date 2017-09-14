08459c72dffa1c8dcc5a568af04bd5a5d14b9de5
CyberJ0 <mir.govna@mail.ru>
2017-09-15 01:31:31 +0200

    Копирование некоторых переменных из массива(а) в массив(b)

--- src/lessons/CopyFromArrayToArray.java
+++ src/lessons/CopyFromArrayToArray.java
@@ -7,9 +7,9 @@ public class CopyFromArrayToArray {
         // копирование начинается со второй позиции smailPrimes (в исходном массиве),
         // копируемые элементы помещаются luckyNumbers (в целевой массив), начиная с третьей позиции
 
-        int[] smailPrimes = {2, 3, 5, 7, 11, 13};
+        int[] smallPrimes = {2, 3, 5, 7, 11, 13};
         int[] luckyNumbers = {1001, 1002, 1003, 1004, 1005, 1006, 1007};
-        System.arraycopy(smailPrimes, 2, luckyNumbers, 3, 4);
+        System.arraycopy(smallPrimes, 2, luckyNumbers, 3, 4);
         for (int i = 0; i < luckyNumbers.length; i++)
             System.out.println(i + ": " + luckyNumbers[i]);
     }
