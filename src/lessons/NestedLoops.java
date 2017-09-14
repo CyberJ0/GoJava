327aa37dc1bd05c1c7dd78bda7fb077ce74fd952
CyberJ0 <mir.govna@mail.ru>
2017-09-14 23:54:39 +0200

    Вложенные циклы - горки из звездочек '*'

---
+++ src/lessons/NestedLoops.java
@@ -0,0 +1,23 @@
+package lessons;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println("");
+        }
+
+        // разделитель
+        System.out.println("");
+
+        /** в обратную сторону */
+        for (int i = 0; i < 10; i++) {
+            for (int j = 10; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
