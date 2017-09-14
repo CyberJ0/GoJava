15ca9ac487642b65dcfa201d235f3cfbfa3e0a9b
CyberJ0 <mir.govna@mail.ru>
2017-09-14 23:48:02 +0200

    Средняя арифметическая сумма массива

---
+++ src/lessons/ArithmeticMean.java
@@ -0,0 +1,15 @@
+package lessons;
+
+public class ArithmeticMean {
+    public static void main(String[] args) {
+        int [] mice = {8,4,1,5,7};
+        int result = 0;
+
+        for (int i = 0; i < mice.length; i++) {
+            result += mice[i];
+            System.out.println(result);
+        }
+        result /= 5;
+        System.out.println("Среднее арифметическое: " + result);
+    }
+}
