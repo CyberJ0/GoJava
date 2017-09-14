354dd87483af79f170bacb3c6327ed538a635cc7
CyberJ0 <mir.govna@mail.ru>
2017-09-14 23:52:07 +0200

    Сумма массива циклом - foreach

---
+++ src/lessons/SumCycleForeach.java
@@ -0,0 +1,13 @@
+package lessons;
+
+public class SumCycleForeach {
+    public static void main(String[] args) {
+        int[] num = {1,2,3};
+        int sum = 0;
+        for (int key : num) {
+            sum += key;
+            // System.out.println("Цикл: " + sum);
+        }
+        System.out.println("Сумма чисел: " + sum);
+    }
+}
