3bd4aebbc284a2c03ab0708d70182de6cc11d5f9
CyberJ0 <mir.govna@mail.ru>
2017-09-15 00:01:41 +0200

    Рандом - случайные 10 цифр в промежутке от 0 до 99 (легко)

---
+++ src/lessons/RandomArrayGenerator.java
@@ -0,0 +1,15 @@
+package lessons;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomArrayGenerator {
+    public static void main(String[] args) {
+        int [] arr = new int[10]; // массив 10 на 10
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100); // граница рандомизации чисел 0-99
+        }
+        System.out.println(Arrays.toString(arr)); // вывод массива осуществляется коммандой Arrays.toString
+    }
+}
