11b17aee3b8d57ff8a350aeafcc0fedd8a2f6fb2
CyberJ0 <mir.govna@mail.ru>
2017-09-16 19:31:04 +0200

    Ввод размерности массива, задать значения, вывод массива и его значений

---
+++ src/lessons/Scanner/EnterNumArrayExitArrayValue.java
@@ -0,0 +1,22 @@
+package lessons.Scanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EnterNumArrayExitArrayValue {
+    public static void main(String[] args) {
+
+        int[] a;
+        int b;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter num of array: ");
+        b = scan.nextInt();
+        a = new int[b];
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Enter a[" + i + "] = ");
+            a[i] = scan.nextInt();
+        }
+        System.out.println("Введен массив: " + Arrays.toString(a));
+    }
+}
