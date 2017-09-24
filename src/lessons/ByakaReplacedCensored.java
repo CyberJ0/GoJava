797dfb87bb8640307886987a023651613567059f
CyberJ0 <mir.govna@mail.ru>
2017-09-24 16:12:32 +0200

    Замена бяка на censored (replace)

---
+++ src/lessons/ByakaReplacedCensored.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class ByakaReplacedCensored {
+    public static void main(String[] args) {
+        //Задача:
+        //Необходимо слово - бяка, заменить на - censored
+        String cens = "Какая то бяка, была настолько бяка, что эту бяка не исправить.";
+        System.out.println(cens);
+        cens = cens.replace("бяка", "censored");
+        System.out.println(cens);
+    }
+}
