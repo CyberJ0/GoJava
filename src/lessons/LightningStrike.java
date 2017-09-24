06f02c04dfa3ec63d3d9bc47f6d21139c61a3a36
CyberJ0 <mir.govna@mail.ru>
2017-09-24 16:18:09 +0200

    Расчитать расстояние до удара молнии. Перевести футы в метры.

---
+++ src/lessons/LightningStrike.java
@@ -0,0 +1,25 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class LightningStrike {
+    public static void main(String[] args) {
+// Задача:
+// Рассчитать расстояния до места удара молнии. Звук в воздухе распространяется
+// со скоростью приблизительно равной 1100 футам в секунду.
+// Зная интервал времени между вспышкой молнии и звуком сопровождающим ее
+// можно рассчитать расстояние. Допустим интервал 7,2 секунды.
+
+        double a, b, c;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введите секунды после вспышки: ");
+        b = scn.nextDouble();
+        System.out.print("Введите скорость распространения звука в футах: ");
+        a = scn.nextDouble();
+        c = b * a;
+        System.out.println("Расстояние до удара молнии равняется: " + c + " футам");
+        // перевод футов в метры
+        double metr = c * 0.3048;
+        System.out.println("В " + c + " фут., " + metr + " м.");
+    }
+}
