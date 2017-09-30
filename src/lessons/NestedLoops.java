134c701f637a765df554f290b4837fbe87d7f7ec
CyberJ0 <mir.govna@mail.ru>
2017-09-30 22:27:18 +0200

    Треугольники из * верхний и нижний

--- src/lessons/NestedLoops.java
+++ src/lessons/NestedLoops.java
@@ -2,6 +2,8 @@ package lessons;
 
 public class NestedLoops {
     public static void main(String[] args) {
+        // треугольники из *
+
         for (int i = 1; i < 11; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print('*');
