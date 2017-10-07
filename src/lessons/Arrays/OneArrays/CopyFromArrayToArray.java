768c016d636a9b1f95bdd0dc67fb20e9c02fe11f
CyberJ0 <mir.govna@mail.ru>
2017-10-07 23:17:49 +0200

    Копируем часть из одного массива в другой массив

--- src/lessons/Arrays/OneArrays/CopyFromArrayToArray.java
+++ src/lessons/Arrays/OneArrays/CopyFromArrayToArray.java
@@ -3,12 +3,12 @@ package lessons.Arrays.OneArrays;
 public class CopyFromArrayToArray {
     public static void main(String[] args) {
 
-        // Операторы создают два массива, затем коируют последние 4 элемента первого массива во второй.
+        // Операторы создают два массива, затем копируют последние 4 элемента первого массива во второй.
         // копирование начинается со второй позиции smailPrimes (в исходном массиве),
-        // копируемые элементы помещаются luckyNumbers (в целевой массив), начиная с третьей позиции
+        // копируемые элементы помещаются в luckyNumbers (в целевой массив), начиная с третьей позиции
 
-        int[] smallPrimes = {2, 3, 5, 7, 11, 13};
-        int[] luckyNumbers = {1001, 1002, 1003, 1004, 1005, 1006, 1007};
+        int[] smallPrimes = {2, 3, 5, 7, 11, 13}; // 6 позиций
+        int[] luckyNumbers = {1001, 1002, 1003, 1004, 1005, 1006, 1007}; // 7 позиций
         System.arraycopy(smallPrimes, 2, luckyNumbers, 3, 4);
         for (int i = 0; i < luckyNumbers.length; i++)
             System.out.println(i + ": " + luckyNumbers[i]);
