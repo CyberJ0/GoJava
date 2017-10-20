fbcf8907cc53bc2c743a76e5b718d112ce05c5c0
CyberJ0 <mir.govna@mail.ru>
2017-10-20 22:22:04 +0200

    Таблица умножения

--- src/lessons/Other/MultiplicationTable.java
+++ src/lessons/Other/MultiplicationTable.java
@@ -5,7 +5,7 @@ public class MultiplicationTable {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 System.out.printf("%5d", (i + 1) * (j + 1)); // форматированная строка вывода printf
-                // %5d = В десятеричном. Минимальная ширина строки 5 знаков.
+            // %5d = В десятеричном. Минимальная ширина строки 5 знаков.
             }
             System.out.println();
         }
