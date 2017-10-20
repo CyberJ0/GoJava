a39ab3f187b68d4ebd85dcdf5828a0481f7a75af
CyberJ0 <mir.govna@mail.ru>
2017-10-20 22:35:00 +0200

    Cреднее арифметическое

--- src/lessons/Other/ArithmeticMean.java
+++ src/lessons/Other/ArithmeticMean.java
@@ -2,14 +2,15 @@ package lessons.Other;
 
 public class ArithmeticMean {
     public static void main(String[] args) {
-        int [] mice = {8,4,1,5,7};
+        int[] mice = {8, 4, 1, 5, 7};
         int result = 0;
 
         for (int i = 0; i < mice.length; i++) {
             result += mice[i];
             System.out.println(result);
         }
-        result /= 5;
-        System.out.println("Среднее арифметическое: " + result);
+        System.out.println("Среднее арифметическое: " + (result /= 5));
+        // 8 + 4 + 1 + 5 + 7 = 25;
+        // 25 / 5 = 5;
     }
 }
