78e0635a0a9e097243d4e74847492792a6f28e60
CyberJ0 <mir.govna@mail.ru>
2017-10-20 22:20:23 +0200

    Треугольники вверх и вниз из *

--- src/lessons/Other/NestedLoops.java
+++ src/lessons/Other/NestedLoops.java
@@ -3,14 +3,12 @@ package lessons.Other;
 public class NestedLoops {
     public static void main(String[] args) {
         // треугольники из *
-
         for (int i = 1; i < 11; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print('*');
             }
             System.out.println("");
         }
-
         // разделитель
         System.out.println("");
 
