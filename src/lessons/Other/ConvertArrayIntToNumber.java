62cba4faee0e9bf7f4fec9bf08014faa81e28296
CyberJ0 <mir.govna@mail.ru>
2018-01-11 22:55:00 +0200

    Преобразовать массив чисел в одно число

--- src/lessons/Other/ConvertArrayIntToNumber.java
+++ src/lessons/Other/ConvertArrayIntToNumber.java
@@ -1,6 +1,6 @@
 package lessons.Other;
 
-public class ConvertArrayIntToString {
+public class ConvertArrayIntToNumber {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0};
         long res = 0;
