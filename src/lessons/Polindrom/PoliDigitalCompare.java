adc6842c375fac81a0530365789d099764212a7c
CyberJ0 <mir.govna@mail.ru>
2018-01-11 22:19:37 +0200

    Polindrom - сравнение положительных и отрицательных чисел

--- src/lessons/Polindrom/PoliDigitalCompare.java
+++ src/lessons/Polindrom/PoliDigitalCompare.java
@@ -2,19 +2,18 @@ package lessons.Polindrom;
 
 public class PoliDigitalCompare {
     public static void main(String[] args) {
-        int[] a = {34, 123, -343, 59, -121, 797, -454};
-
+        int[] a = {17, -616, 123, 787, 49, 71, 321, -555};
+        int ikey = 0;
         for (int key : a) {
+            ikey = key;
             System.out.print(key + ", ");
             int result = 0;
-            while ((key > 0) || (key < 0)){
+            while (key != 0) {
                 result = result * 10 + key % 10;
                 key = key / 10;
 
-                for (int ke : a) {
-                    if (ke == result) {
-                        System.out.println("-> полиндром");
-                    }
+                if (ikey == result) {
+                    System.out.println("-> полиндром");
                 }
             }
         }
